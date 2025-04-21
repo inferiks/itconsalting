6fd4134bc6e93b8aea2dbc7324a2b75be8fbad55
weylor654 <tsylyurik22@bk.ru>
2025-04-21 22:26:31 +0300

    updated information on the 'About Us' page

--- itconsalting/settings.py
+++ itconsalting/settings.py
@@ -118,7 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-
+STATICFILES_DIRS = [
+    BASE_DIR / 'main' / 'static',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
